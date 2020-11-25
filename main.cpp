a61899dc0e345e259b495176c03e96ff92228b21
Thomas Medioni <medioni.thomas@gmail.com>
2020-11-25 23:07:51 +0100

    Perform call to CDN

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+
 #include <httplib.h>
 #include <spdlog/spdlog.h>
+
+
 
 namespace {
 
@@ -14,22 +18,25 @@ namespace {
         }
     };
 
-    const std::regex path_prefix_regex(R"reg(^http(s)?:\/\/[^\/]+\/)reg");
-
+    const std::regex path_prefix_regex(R"reg(^http(s)?:\/\/([^\/]+)((\/[^\/]+)*)\/([^\/]+)$)reg");
+    using namespace std::string_literals;
     class Proxy
     {
     public:
         explicit Proxy(const std::string& url)
         {
-            std::smatch url_prefix;
-            if (std::regex_search(url, url_prefix,path_prefix_regex))
+            std::smatch url_matches;
+            if (std::regex_match(url, url_matches,path_prefix_regex))
             {
-                if (url_prefix.suffix().length() == 0)
-                    throw InvalidStreamUrlException();
+                // First capture group is the s from https
+                m_domain = url_matches[2];
 
-                m_path = url_prefix[0];
-                m_resource = url_prefix.suffix();
-                spdlog::trace("Parsed input stream address: '{}' from '{}'", m_path, url);
+                m_path = url_matches[3];
+                m_path.push_back('/');
+
+                m_resource = url_matches[4];
+                spdlog::trace("Parsed input stream CDN domain: '{}' from '{}'", m_domain, url);
+                spdlog::trace("Parsed input stream intermediate path: '{}' from '{}'", m_path, url);
                 spdlog::trace("Parsed input stream resource: '{}' from '{}'", m_resource, url);
             }
             else
@@ -38,14 +45,35 @@ namespace {
             }
         }
 
-        void requestHandler(const httplib::Request &req, httplib::Response &res)
+        void requestHandler(const httplib::Request &request, httplib::Response &result) const
         {
-            spdlog::trace("Request path: '{}'", req.path);
-            spdlog::trace("Request body: '{}'", req.body);
-            res.set_content("", "text/plain");
+            spdlog::trace("Request path: '{}'", request.path);
+            spdlog::trace("Request body: '{}'", request.body);
+
+            httplib::SSLClient client(m_domain.c_str());
+            if (auto subrequest =  client.Get(m_resource.c_str()))
+            {
+                if (subrequest->status == 200)
+                {
+                    spdlog::trace("Subrequest content: {}", subrequest->body);
+                }
+                else
+                {
+                    spdlog::error("Subrequest to CDN error: {}", subrequest.error());
+                }
+
+            }
+            else
+            {
+                spdlog::error("Subrequest to CDN error: {}", subrequest.error());
+            }
+
+
+            result.set_content("", "text/plain");
         }
 
     private:
+        std::string m_domain;
         std::string m_path;
         std::string m_resource;
     };
